a80bab8e14c5fdd56e9d4e710bf9782a8ec377cc
Saicharan-Pallantla-2004 <154867145+Saicharan-Pallantla-2004@users.noreply.github.com>
2024-05-15 20:23:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ $rand_keys = ($num_elements >= 10) ? array_rand($url_data, 10) : array_rand($url
                 <ul>
                     <li><a href="home_demo.php"><button id="nb1">Home</button></a></li>
                     <li><a href="about_us.html"><button id="nb2">About Us!</button></a></li>
-                    <li><a href="Login.php"><button id="nb3">Login</button></a></li>
+                    <li><a href="/Login/Login.php"><button id="nb3">Login</button></a></li>
                 </ul>
             </div>
         </nav>
